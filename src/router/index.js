3dc6cac258c0a6de71f332c3452a954e18716af3
xiaoxiaoweii <2679656468@qq.com>
2019-12-10 16:42:58 +0800

    首页-以前的有点乱

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
-import City from '@/pages/city/City'
 
 Vue.use(Router)
 
@@ -11,10 +10,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    }, {
-      path: '/city',
-      name: 'City',
-      component: City
     }
   ]
 })
